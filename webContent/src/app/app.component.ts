7f213497407753d6a475f05af3bf4b64ab774489
Vignesh Lakshmanan <vigneshlaxman91@gmail.com>
2018-02-15 17:56:39 +0530

    Creating multiple components

--- webContent/src/app/app.component.ts
+++ webContent/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wpl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
